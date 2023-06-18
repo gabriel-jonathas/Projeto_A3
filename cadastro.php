c8a44ba39860c19149fb2284ed1fa11ec30b486b
GABRIEL OLIVEIRA <127751781+gabriel-jonathas@users.noreply.github.com>
2023-06-18 18:46:13 -0300

    Add files via upload

---
+++ cadastro.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<center>
+    <h1>cadastro<h1>
+        <form id="cadastro" action="cadastrar.php" method="post">
+            nome: <input type="text" name="nome" required><br>
+            login: <input type="text" name="login" required><br>
+            senha: <input type="password" name="senha" required><br>
+            <input type="submit" name="cadastrar" value="cadastrar">
+        </form>
+<center>
+
+</body>
+</html>
